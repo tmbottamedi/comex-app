6902a5a1dcdde208efb663fffba194f1dfbd4f78
Tiago Mendes Bottamedi <tmbottamedi@mppr.mp.br>
2025-09-17 13:55:49 -0300

    Adaptação de CategoriaDao e ProdutoDao para Jpa

--- src/main/java/br/com/alura/comex/repository/CategoriaRepository.java
+++ src/main/java/br/com/alura/comex/repository/CategoriaRepository.java
@@ -1,8 +1,10 @@
 package br.com.alura.comex.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import br.com.alura.comex.model.Categoria;
 
+@Repository
 public interface CategoriaRepository extends JpaRepository<Categoria, Long> {
 }
--- src/main/java/br/com/alura/comex/repository/ProdutoRepository.java
+++ src/main/java/br/com/alura/comex/repository/ProdutoRepository.java
@@ -1,8 +1,10 @@
 package br.com.alura.comex.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import br.com.alura.comex.model.Produto;
 
+@Repository
 public interface ProdutoRepository extends JpaRepository<Produto, Long> {
 }
